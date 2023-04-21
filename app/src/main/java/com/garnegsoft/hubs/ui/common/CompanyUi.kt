db80d23bd7bcadc27aeacf76a422fa648be6964e
daniil <tisin371@gmail.com>
2023-04-21 22:03:42 +0200

    Fix wrong title text style weight for company card

--- app/src/main/java/com/garnegsoft/hubs/ui/common/CompanyUi.kt
+++ app/src/main/java/com/garnegsoft/hubs/ui/common/CompanyUi.kt
@@ -120,7 +120,7 @@ data class CompanyCardStyle(
     val avatarSize: Dp = 40.dp,
     val avatarShape: Shape = RoundedCornerShape(10.dp),
     val titleTextStyle: TextStyle = TextStyle.Default.copy(
-        fontWeight = FontWeight.W500,
+        fontWeight = FontWeight.W600,
         fontSize = 20.sp
     ),
     val descriptionTextStyle: TextStyle = TextStyle.Default.copy(color = Color.Gray),
