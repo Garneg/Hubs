5bf6888675169535cfc5d950ec39ac0415adb77a
daniil <tisin371@gmail.com>
2023-05-27 22:01:13 +0200

    some settings screen drafts

--- app/src/main/java/com/garnegsoft/hubs/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/garnegsoft/hubs/ui/screens/settings/SettingsScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -23,12 +23,12 @@ fun SettingsScreen() {
         Scaffold(
             topBar = {
                 TopAppBar(
-                    title = { Text("Settings") },
+                    title = { Text("Настройки") },
                     navigationIcon = {
                         IconButton(onClick = { /*TODO*/ }) {
                             Icon(
                                 imageVector = Icons.Default.ArrowBack,
-                                contentDescription = "Go back"
+                                contentDescription = "Назад"
                             )
                         }
                     }
@@ -47,22 +47,32 @@ fun SettingsScreen() {
                         .fillMaxWidth()
                         .clip(RoundedCornerShape(10.dp))
                         .clickable { }
-                        .padding(14.dp)) {
-                        Text("Dark theme")
+                        .padding(vertical = 14.dp, horizontal = 4.dp)) {
+                        Text("Темная тема")
                     }
                     Row(modifier = Modifier
                         .fillMaxWidth()
                         .clip(RoundedCornerShape(10.dp))
                         .clickable { }
-                        .padding(14.dp)) {
-                        Text("Dark theme")
+                        .padding(vertical = 14.dp, horizontal = 4.dp)) {
+                        Text("Показывать изображения в ленте")
+                    }
+
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp))
+                        .clickable { }
+                        .padding(vertical = 14.dp, horizontal = 4.dp)) {
+                        Text(modifier = Modifier.weight(1f), text = "Показывать отрывок статьи в ленте")
+                        var isChecked by remember { mutableStateOf(false) }
+                        Checkbox(checked = isChecked, onCheckedChange = { isChecked = it})
                     }
                     Row(modifier = Modifier
                         .fillMaxWidth()
                         .clip(RoundedCornerShape(10.dp))
                         .clickable { }
-                        .padding(14.dp)) {
-                        Text("Dark theme")
+                        .padding(vertical = 14.dp, horizontal = 4.dp)) {
+                        Text("Количество строк отрывка статьи в ленте")
                     }
 
                 }
