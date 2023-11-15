fbe087f9bebd196f771ae787ef66b325ee09bd3a
daniil <tisin371@gmail.com>
2023-11-15 23:14:36 +0100

    Add slider for configuring text snippet max lines

--- app/src/main/java/com/garnegsoft/hubs/ui/screens/settings/FeedSettingsScreen.kt
+++ app/src/main/java/com/garnegsoft/hubs/ui/screens/settings/FeedSettingsScreen.kt
@@ -14,11 +14,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.Checkbox
@@ -29,6 +34,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RangeSlider
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -45,7 +51,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -59,6 +70,7 @@ import com.garnegsoft.hubs.ui.common.feedCards.article.ArticleCard
 import com.garnegsoft.hubs.ui.common.feedCards.article.ArticleCardStyle
 import com.garnegsoft.hubs.ui.screens.settings.cards.SettingsCardItem
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 
 class FeedSettingsScreenViewModel : ViewModel() {
@@ -91,6 +103,12 @@ class FeedSettingsScreenViewModel : ViewModel() {
 			)
 		}
 	}
+	
+	fun ChangeSnippetMaxLinesSetting(context: Context, lines: Int) {
+		viewModelScope.launch {
+			HubsDataStore.Settings.edit(context, HubsDataStore.Settings.ArticleCard.TextSnippetMaxLines, lines)
+		}
+	}
 }
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -103,6 +121,7 @@ fun FeedSettingsScreen(
 	
 	val scaffoldState = rememberBottomSheetScaffoldState()
 	BottomSheetScaffold(
+		modifier = Modifier.imePadding(),
 		scaffoldState = scaffoldState,
 		topBar = {
 			TopAppBar(
@@ -135,8 +154,9 @@ fun FeedSettingsScreen(
 				Column(
 					modifier = Modifier
 						.fillMaxWidth()
+						.verticalScroll(rememberScrollState())
 						.padding(horizontal = 16.dp)
-						.verticalScroll(rememberScrollState()),
+						.padding(bottom = 20.dp),
 					verticalArrangement = Arrangement.spacedBy(4.dp)
 				) {
 					val showImage by HubsDataStore.Settings.getValueFlow(
@@ -195,6 +215,33 @@ fun FeedSettingsScreen(
 						
 						
 					}
+					
+					val snippetMaxLines by HubsDataStore.Settings.getValueFlow(
+						LocalContext.current,
+						HubsDataStore.Settings.ArticleCard.TextSnippetMaxLines
+					).collectAsState(initial = null)
+					
+					snippetMaxLines?.let {
+						Column(
+							modifier = Modifier.padding(4.dp)
+						) {
+							var sliderValue by remember { mutableStateOf(it.toFloat()) }
+							
+							Text(text = "Показывать строк начала статьи: ${"%.0f".format(sliderValue)}",)
+							Slider(
+								value = sliderValue,
+								enabled = showTextSnippet ?: false,
+								valueRange = 2f..10f,
+								steps = 7,
+								onValueChange = {
+									sliderValue = it
+								},
+								onValueChangeFinished = {
+									viewModel.ChangeSnippetMaxLinesSetting(context, sliderValue.toInt())
+								}
+							)
+						}
+					}
 				}
 				
 			}
@@ -206,16 +253,17 @@ fun FeedSettingsScreen(
 		Column(
 			modifier = Modifier.padding(it)
 		) {
+			val fakeArticles = remember { listOf(TestArticle, SecondArticle) }
 			ArticleCardStyle.defaultArticleCardStyle()?.let { style ->
 				LazyColumn(
 					contentPadding = PaddingValues(8.dp),
 					verticalArrangement = Arrangement.spacedBy(8.dp)
 				) {
-					items(20) {
+					items(fakeArticles) {
 						
 						Box(modifier = Modifier.animateContentSize()) {
 							ArticleCard(
-								article = TestArticle,
+								article = it,
 								onClick = { /*TODO*/ },
 								onAuthorClick = { /*TODO*/ },
 								onCommentsClick = { /*TODO*/ },
@@ -242,9 +290,9 @@ val TestArticle = ArticleSnippet(
 	listOf(),
 	Article.Author(
 		"squada",
-		avatarUrl = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+		avatarUrl = "https://assets.habr.com/habr-web/img/avatars/012.png"
 	),
-	Article.Statistics(0, 0, 0, 0, 0, 0),
+	Article.Statistics(5, 9, 2000, 4, 0, 0),
 	listOf(
 		Article.Hub("", true, false, "Программирование", null),
 		Article.Hub("", true, false, ".NET", Article.Hub.RelatedData(true)),
@@ -253,8 +301,34 @@ val TestArticle = ArticleSnippet(
 	"""Как мне дальше двигаться по карьере? - такой вопрос многие задавали себе как в начале своего пути так и позже не только в IT, но и в других сферах. Спектор этого вопроса охватывает почти все работящее население планеты, ведь карьера для человека сегодня дело очень серъезное бла бла мне надоело писать эту заглушку, извините.""".trimMargin(),
 	"https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663365833_63-mykaleidoscope-ru-p-pozdravlenie-s-povisheniem-vkontakte-65.jpg",
 	null,
-	5,
+	15,
 	PostComplexity.Low,
+	null,
+	false
+)
+
+val SecondArticle = ArticleSnippet(
+	0,
+	"вчера, 03:28",
+	false,
+	"Кажется, мы слишком сильно ускорились",
+	EditorVersion.FirstVersion,
+	PostType.Unknown,
+	listOf(),
+	Article.Author(
+		"gohonor",
+		avatarUrl = "https://assets.habr.com/habr-web/img/avatars/016.png"
+	),
+	Article.Statistics(45, 12, 4000, -14, 0, 0),
+	listOf(
+		Article.Hub("", true, false, "Программирование", null),
+		Article.Hub("", true, false, "Rust", Article.Hub.RelatedData(true))
+	),
+	"Перейдя на новый проект, который в компании писали тогда уже год на языке программирования Rust, и посмотрев как работают мои коллеги, стараясь применить каждую оптимизацию для более быстрого вычисления, я понял, что то не так, кажется мы слишком ускорили наш проект.",
+	"https://i.ytimg.com/vi/YEVdOWsg6ik/maxresdefault.jpg",
+	null,
+	10,
+	PostComplexity.None,
 	null,
 	false
 )
