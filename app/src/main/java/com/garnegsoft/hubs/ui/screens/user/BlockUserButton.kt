c991cafdf335a265edd2ad2853a7f8ea3b9c182e
daniil <tisin371@gmail.com>
2024-07-09 01:47:48 +0200

    Add animated icon for block user button

--- app/src/main/java/com/garnegsoft/hubs/ui/screens/user/BlockUserButton.kt
+++ app/src/main/java/com/garnegsoft/hubs/ui/screens/user/BlockUserButton.kt
@@ -1,8 +1,11 @@
 package com.garnegsoft.hubs.ui.screens.user
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -12,29 +15,26 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.garnegsoft.hubs.R
-import com.garnegsoft.hubs.data.user.UserController
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
 fun BlockUserButton(
 	blocked: Boolean,
@@ -60,19 +60,15 @@ fun BlockUserButton(
 			modifier = Modifier.align(Alignment.Center),
 			verticalAlignment = Alignment.CenterVertically
 		) {
-			if (blocked){
+			val animatedIcon = AnimatedImageVector.animatedVectorResource(id = R.drawable.add_block_animated)
+			
 				Icon(
-					painter = painterResource(id = R.drawable.add_circle),
+					modifier = Modifier.size(24.dp),
+					painter = rememberAnimatedVectorPainter(animatedImageVector = animatedIcon, atEnd = !blocked),
 					tint = contentColor,
 					contentDescription = null
 				)
-			} else {
-				Icon(
-					painter = painterResource(id = R.drawable.block),
-					tint = contentColor,
-					contentDescription = null
-				)
-			}
+			
 			Spacer(modifier = Modifier.width(4.dp))
 			
 			Text(
