fe8ba0b3c52e8ebac28a4d6d91817e4e7a8a58f0
daniil <tisin371@gmail.com>
2024-08-08 02:32:02 +0200

    fix merging issues

--- app/src/main/java/com/garnegsoft/hubs/data/dataStore/FilterSavingController.kt
+++ app/src/main/java/com/garnegsoft/hubs/data/dataStore/FilterSavingController.kt
@@ -1,8 +1,7 @@
-package com.garnegsoft.hubs.api.dataStore
+package com.garnegsoft.hubs.data.dataStore
 
 import android.content.Context
 import android.util.Log
-import com.garnegsoft.hubs.data.dataStore.HubsDataStore
 import com.garnegsoft.hubs.ui.screens.main.ArticlesFilter
 import com.garnegsoft.hubs.ui.screens.main.MyFeedFilter
 import com.garnegsoft.hubs.ui.screens.main.NewsFilter
