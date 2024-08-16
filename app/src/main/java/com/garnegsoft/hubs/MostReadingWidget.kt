6b74fb0a7a41cb026fc01fd35c0de1f731419bfd
daniil <tisin371@gmail.com>
2024-08-16 20:37:39 +0200

    Move widget updating worker to its own file

--- app/src/main/java/com/garnegsoft/hubs/MostReadingWidget.kt
+++ app/src/main/java/com/garnegsoft/hubs/MostReadingWidget.kt
@@ -48,17 +48,3 @@ class MostReadingWidget : GlanceAppWidget() {
 	
 }
 
-class MostReadingWidgetUpdateWorker(
-	val context: Context,
-	params: WorkerParameters
-) : CoroutineWorker(context, params) {
-	override suspend fun doWork(): Result {
-		val widgetManager = GlanceAppWidgetManager(context)
-		val widgets = widgetManager.getGlanceIds(MostReadingWidget::class.java)
-		Log.i("most_reading_widget", "Count of widget to update: ${widgets.size}")
-		MostReadingWidget().updateAll(context)
-		
-		return Result.success()
-	}
-	
-}
