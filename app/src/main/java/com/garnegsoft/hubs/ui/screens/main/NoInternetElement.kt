dc25461b8f524538aeb4c2e95b00beab27a99269
daniil <tisin371@gmail.com>
2024-09-07 03:15:10 +0200

    Add icons for no internet connection screen buttons

--- app/src/main/java/com/garnegsoft/hubs/ui/screens/main/NoInternetElement.kt
+++ app/src/main/java/com/garnegsoft/hubs/ui/screens/main/NoInternetElement.kt
@@ -4,19 +4,26 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.garnegsoft.hubs.R
 
 
 @Composable
@@ -40,9 +47,13 @@ fun NoInternetElement(
 				textAlign = TextAlign.Center)
 			Spacer(modifier = Modifier.height(32.dp))
 			Button(onClick = onSavedArticles, elevation = null) {
+				Icon(painter = painterResource(id = R.drawable.download_done), contentDescription = null)
+				Spacer(modifier = Modifier.width(8.dp))
 				Text(text = "Скачанные статьи")
 			}
 			TextButton(onClick = onTryAgain) {
+				Icon(imageVector = Icons.Default.Refresh, contentDescription = null)
+				Spacer(modifier = Modifier.width(8.dp))
 				Text(text = "Попробовать еще раз")
 			}
 		}
