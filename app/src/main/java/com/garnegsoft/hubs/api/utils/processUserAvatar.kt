e75715cea5eae4a5f001e6d08d8107a464c56445
Garneg <tisin371@gmail.com>
2024-12-11 01:22:08 +0100

    Fix processUserAvatar does not return placeholder avatar url when avatarUrl is empty, but not null

--- app/src/main/java/com/garnegsoft/hubs/api/utils/processUserAvatar.kt
+++ app/src/main/java/com/garnegsoft/hubs/api/utils/processUserAvatar.kt
@@ -4,5 +4,11 @@ package com.garnegsoft.hubs.api.utils
 /**
  * Replaces uncorrect host of habrastorage and returns url to placeholder avatar in case avatarUrl is null
  */
-fun processUserAvatar(avatarUrl: String?, alias: String): String =
-    avatarUrl?.replace("//habrastorage", "https://hsto") ?: placeholderAvatarUrl(alias)
+fun processUserAvatar(avatarUrl: String?, alias: String): String {
+    return if (avatarUrl == null || avatarUrl == "") {
+        placeholderAvatarUrl(alias)
+    } else {
+        avatarUrl.replace("//habrastorage", "https://hsto")
+    }
+}
+
