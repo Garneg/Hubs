56f536bec591fe7894c3f01aec915e7c26eee75c
Garneg <tisin371@gmail.com>
2025-10-06 20:38:07 +0200

    Add animation to select theme mode menu appearance on the screen

--- app/src/main/java/com/garnegsoft/hubs/ui/screens/settings/cards/AppearanceSettingsCard.kt
+++ app/src/main/java/com/garnegsoft/hubs/ui/screens/settings/cards/AppearanceSettingsCard.kt
@@ -2,6 +2,8 @@ package com.garnegsoft.hubs.ui.screens.settings.cards
 
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,7 +12,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
@@ -21,6 +25,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -30,8 +35,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
@@ -81,76 +88,120 @@ fun AppearanceSettingsCard(
 							}
 						)
 						Spacer(modifier = Modifier.width(4.dp))
-						
+
 						Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
-						
+
 					}
-					if (showSelectThemeMenu) {
+					val showMenuTransition = updateTransition(showSelectThemeMenu)
+
+					val scaleFactor by showMenuTransition.animateFloat { if (it) 1f else 0f }
+					if (showSelectThemeMenu || showMenuTransition.currentState || showMenuTransition.targetState) {
 						Popup(
 							onDismissRequest = {
 								showSelectThemeMenu = false
 							},
 							properties = PopupProperties(true)
 						) {
-							BaseMenuContainer(modifier = Modifier.padding(end = 20.dp)) {
-								Row(
-									modifier = Modifier
-										.fillMaxWidth()
-										.heightIn(48.dp)
-										.clickable {
-											viewModel.setTheme(
-												context,
-												HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.SystemDefined
-											)
-											showSelectThemeMenu = false
-										}
-										.padding(horizontal = 16.dp),
-									verticalAlignment = Alignment.CenterVertically
+							Box(
+								modifier = Modifier
+									.offset {
+										IntOffset(x = 8.dp.roundToPx(), y = 0)
+									}
+									.graphicsLayer {
+										alpha = scaleFactor
+										translationY = -(size.height - (size.height * scaleFactor))/2
+										scaleY = scaleFactor
+									}
 								) {
-									Text(
-										text = "Системная",
-										style = MaterialTheme.typography.body1
-									)
-								}
-								Row(
-									modifier = Modifier
-										.fillMaxWidth()
-										.heightIn(48.dp)
-										.clickable {
-											viewModel.setTheme(
-												context,
-												HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.Light
+								BaseMenuContainer(modifier = Modifier.padding(end = 32.dp)) {
+									Row(
+										modifier = Modifier
+											.fillMaxWidth()
+											.heightIn(48.dp)
+											.clickable {
+												viewModel.setTheme(
+													context,
+													HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.SystemDefined
+												)
+												showSelectThemeMenu = false
+											}
+											.padding(horizontal = 16.dp),
+										verticalAlignment = Alignment.CenterVertically
+									) {
+										Text(
+											modifier = Modifier.weight(1f),
+											text = "Системная",
+											style = MaterialTheme.typography.body1
+										)
+										if (themeMode == HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.SystemDefined ||
+											themeMode == HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.Undetermined
+										) {
+											Icon(
+												modifier = Modifier.size(20.dp),
+												imageVector = Icons.Default.Done,
+												contentDescription = "Выбрано"
 											)
-											showSelectThemeMenu = false
 										}
-										.padding(horizontal = 16.dp),
-									verticalAlignment = Alignment.CenterVertically
-								) {
-									Text(
-										text = "Светлая",
-										style = MaterialTheme.typography.body1
-									)
-								}
-								Row(
-									modifier = Modifier
-										.fillMaxWidth()
-										.heightIn(48.dp)
-										.clickable {
-											viewModel.setTheme(
-												context,
-												HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.Dark
+									}
+									Row(
+										modifier = Modifier
+											.fillMaxWidth()
+											.heightIn(48.dp)
+											.clickable {
+												viewModel.setTheme(
+													context,
+													HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.Light
+												)
+												showSelectThemeMenu = false
+											}
+											.padding(horizontal = 16.dp),
+										verticalAlignment = Alignment.CenterVertically
+									) {
+										Text(
+											modifier = Modifier.weight(1f),
+											text = "Светлая",
+											style = MaterialTheme.typography.body1
+										)
+
+										if (themeMode == HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.Light) {
+											Icon(
+												modifier = Modifier.size(20.dp),
+												imageVector = Icons.Default.Done,
+												contentDescription = "Выбрано"
 											)
-											showSelectThemeMenu = false
 										}
-										.padding(horizontal = 16.dp),
-									verticalAlignment = Alignment.CenterVertically
-								) {
-									Text(
-										text = "Тёмная",
-										style = MaterialTheme.typography.body1
-									)
+									}
+									Row(
+										modifier = Modifier
+											.fillMaxWidth()
+											.heightIn(48.dp)
+											.clickable {
+												viewModel.setTheme(
+													context,
+													HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.Dark
+												)
+												showSelectThemeMenu = false
+											}
+											.padding(horizontal = 16.dp),
+										verticalAlignment = Alignment.CenterVertically
+									) {
+										Text(
+											modifier = Modifier.weight(1f),
+											text = "Тёмная",
+											style = MaterialTheme.typography.body1
+										)
+
+										if (themeMode == HubsDataStore.Settings.Theme.ColorSchemeMode.ColorScheme.Dark) {
+											Icon(
+												modifier = Modifier.size(20.dp),
+												imageVector = Icons.Default.Done,
+												contentDescription = "Выбрано"
+											)
+										}
+									}
 								}
 							}
+
 						}
 					}
 				}
@@ -168,11 +219,11 @@ fun AppearanceSettingsCard(
 			) {
 				Column(modifier = Modifier.weight(1f)) {
 					Text(text = "Внешний вид статьи")
-					Text(
-						text = "Размер шрифта, межстрочный интервал и т.д.",
-						fontSize = 11.sp,
-						color = MaterialTheme.colors.onSurface.copy(0.5f)
-					)
+//					Text(
+//						text = "Размер шрифта, межстрочный интервал и т.д.",
+//						fontSize = 11.sp,
+//						color = MaterialTheme.colors.onSurface.copy(0.5f)
+//					)
 				}
 				Icon(
 					modifier = Modifier.padding(12.dp),
