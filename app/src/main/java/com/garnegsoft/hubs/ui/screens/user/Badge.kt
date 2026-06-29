9555631f657b5e98c6b6acf53bacb092e94afaa0
Garneg <tisin371@gmail.com>
2026-06-29 13:43:02 +0200

    Adjust badge popup position provider to clip to anchor bounds instead of window's

--- app/src/main/java/com/garnegsoft/hubs/ui/screens/user/Badge.kt
+++ app/src/main/java/com/garnegsoft/hubs/ui/screens/user/Badge.kt
@@ -4,6 +4,8 @@ import android.graphics.Paint
 import android.util.Log
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateIntOffset
+import androidx.compose.animation.core.animateOffset
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
@@ -12,7 +14,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
@@ -29,16 +30,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.drawscope.inset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalDensity
@@ -82,8 +79,8 @@ fun Badge(badge: User.WhoIs.Badge) {
             if (it) 1f else 0f
         }
 
-        val popupOffsetAnimated by popupTransition.animateDp {
-            if (it) 0.dp else 16.dp
+        val popupOffsetAnimated by popupTransition.animateOffset {
+            if (it) Offset.Zero else Offset(0f, 16f * LocalDensity.current.density)
         }
 
         val popupScaleAnimated by popupTransition.animateFloat(
@@ -105,9 +102,18 @@ fun Badge(badge: User.WhoIs.Badge) {
                         layoutDirection: LayoutDirection,
                         popupContentSize: IntSize
                     ): IntOffset {
-                        popupPointerPosition = anchorBounds.width / 2
+                        val initialPosition = anchorBounds.left - (popupContentSize.width - anchorBounds.width) / 2
+                        var horizontalPosition = initialPosition
+                        popupPointerPosition = popupContentSize.width / 2
+                        if (initialPosition < 0){
+                            horizontalPosition = anchorBounds.left
+                            popupPointerPosition = anchorBounds.width / 2
+                        } else if (initialPosition + popupContentSize.width > windowSize.width) {
+                            horizontalPosition = anchorBounds.right - popupContentSize.width
+                            popupPointerPosition = (popupContentSize.width) - (anchorBounds.width / 2)
+                        }
                         Log.i("popupPointer", "anchorBounds: topCenter:${anchorBounds.topCenter}; topLeft:${anchorBounds.topLeft}")
-                        return IntOffset(anchorBounds.left - (2 * density.density).toInt(), anchorBounds.top - popupContentSize.height + (6 * density.density).toInt())
+                        return IntOffset(horizontalPosition, anchorBounds.top - popupContentSize.height + (6 * density.density).toInt())
                     }
                 }
             ) {
@@ -119,16 +125,14 @@ fun Badge(badge: User.WhoIs.Badge) {
                             scaleX = popupScaleAnimated
                             scaleY = popupScaleAnimated
                             alpha = popupAlphaAnimated
-                            translationY = popupOffsetAnimated.toPx()
-
+                            translationY = popupOffsetAnimated.y
                         }
                         .padding(2.dp)
                         .padding(bottom = 20.dp)
 
                         .drawWithCache {
                             val path = Path().apply {
-                                // TODO: Pointer sometimes is off center because popup sometimes clips to window size, should be noticed
-                                moveTo(popupPointerPosition.toFloat() - 10f * density.density, size.height - 2f)
+                                moveTo(popupPointerPosition.toFloat() - 12f * density.density, size.height - 2f)
                                 relativeLineTo(20f * density.density, 0f)
                                 relativeLineTo(-10f * density.density, 10f * density.density)
                                 this.close()
