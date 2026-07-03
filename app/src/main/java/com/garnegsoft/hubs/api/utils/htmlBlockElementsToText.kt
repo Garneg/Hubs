b4aed73a90ae92ffa06493bdd0f3eca374e55a8a
Garneg <tisin371@gmail.com>
2026-07-03 13:28:46 +0200

    Fix crashing because of old html comments

--- app/src/main/java/com/garnegsoft/hubs/api/utils/htmlBlockElementsToText.kt
+++ app/src/main/java/com/garnegsoft/hubs/api/utils/htmlBlockElementsToText.kt
@@ -6,11 +6,13 @@ import org.jsoup.Jsoup
 fun htmlBlocksToText(html: String): String {
 	val stringBuilder = StringBuilder()
 	val document = Jsoup.parse(html)
-	document.body().children()[0].children().forEach {
-		when(it.tagName()) {
-			"blockquote" ->  stringBuilder.append("*Цитата* ")
-			"img" -> stringBuilder.append("*Изображение* ")
-			else -> stringBuilder.append(it.text())
+	if (document.body().childrenSize() > 0) {
+		document.body().children()[0].children().forEach {
+			when (it.tagName()) {
+				"blockquote" -> stringBuilder.append("*Цитата* ")
+				"img" -> stringBuilder.append("*Изображение* ")
+				else -> stringBuilder.append(it.text())
+			}
 		}
 	}
 	if (stringBuilder.isEmpty()) {
